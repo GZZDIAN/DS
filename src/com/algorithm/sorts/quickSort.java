f09b61a7fa3b304e4ba2f748e35c4ea548520f20
gaozhe <373795878@qq.com>
2021-09-11 23:36:25 +0800

    快速排序

--- src/com/algorithm/sorts/quickSort.java
+++ src/com/algorithm/sorts/quickSort.java
@@ -1,5 +1,7 @@
 package com.algorithm.sorts;
 
+import java.util.Arrays;
+
 public class quickSort {
 
     /*
@@ -28,65 +30,58 @@ public class quickSort {
     * */
 
     public static void main(String[] args) {
-        int[] arr = {5,2,1,4,3,10};
-        int[] nums = sortArray(arr);
-        for (int num : nums) {
-            System.out.println(num);
-        }
-    }
-    public static int[] sortArray(int[] nums) {
-        quickSort(nums);
-        return nums;
-    }
-
-    public static void quickSort(int[] arr) {
+        int[] arr = new int[]{4, 4, 6, 5, 3, 2, 8, 1};
         quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void quickSort(int[] arr, int start, int end) {
-        // 如果区域内的数字少于 2 个，退出递归
-        if (start >= end) return;
-        // 将数组分区，并获得中间值的下标
-        int middle = partition(arr, start, end);
-        // 对左边区域快速排序
-        quickSort(arr, start, middle - 1);
-        // 对右边区域快速排序
-        quickSort(arr, middle + 1, end);
+    public static void quickSort(int[] arr, int startIndex, int endIndex) {
+        // 递归结束条件：startIndex大于或等于endIndex时
+        if (startIndex >= endIndex) {
+            return;
+        }
+        // 得到基准元素位置
+        int pivotIndex = partition(arr, startIndex, endIndex);
+        // 根据基准元素，分成两部分进行递归排序
+        quickSort(arr, startIndex, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, endIndex);
     }
 
-    // 将 arr 从 start 到 end 分区，左边区域比基数小，右边区域比基数大，然后返回中间值的下标
-    public static int partition(int[] arr, int start, int end) {
-        // 取第一个数为基数
-        int pivot = arr[start];
-        // 从第二个数开始分区
-        int left = start + 1;
-        // 右边界
-        int right = end;
-        // left、right 相遇时退出循环
-        while (left < right) {
-            // 找到第一个大于基数的位置
-            while (left < right && arr[left] <= pivot)
-                left++;
-            // 交换这两个数，使得左边分区都小于或等于基数，右边分区大于或等于基数
-            if (left != right) {
-                exchange(arr, left, right);
+
+    /**
+     * 分治（双边循环法）
+     *
+     * @param arr        待交换的数组
+     * @param startIndex 起始下标
+     * @param endIndex   结束下标
+     */
+    private static int partition(int[] arr, int startIndex, int endIndex) {
+        // 取第1个位置（也可以选择随机位置）的元素作为基准元素
+        int pivot = arr[startIndex];
+        int left = startIndex;
+        int right = endIndex;
+
+        while (left != right) {
+            //控制right指针比较并左移
+            while (left < right && arr[right] > pivot) {
                 right--;
             }
+            //控制left指针比较并右移
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+            //交换left和right指针所指向的元素
+            if (left < right) {
+                int p = arr[left];
+                arr[left] = arr[right];
+                arr[right] = p;
+            }
         }
-        // 如果 left 和 right 相等，单独比较 arr[right] 和 pivot
-        if (left == right && arr[right] > pivot)
-            right--;
-        // 将基数和中间数交换
-        if (right != start)
-            exchange(arr, start, right);
-        // 返回中间值的下标
-        return right;
+        //pivot和指针重合点交换
+        arr[startIndex] = arr[left];
+        arr[left] = pivot;
+        return left;
     }
 
-    private static void exchange(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
 
 }
