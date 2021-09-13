b8af534a3d03a223022c5d0fd82b983a20f2318a
gaozhe <373795878@qq.com>
2021-09-13 22:05:30 +0800

    最小栈

---
+++ src/com/algorithm/stack/solution01.java
@@ -0,0 +1,103 @@
+package com.algorithm.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 最小栈
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * <p>
+ * push(x) —— 将元素 x 推入栈中。
+ * pop()—— 删除栈顶的元素。
+ * top()—— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
+ * <p>
+ * <p>
+ * 示例:
+ * <p>
+ * 输入：
+ * ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ * [[],[-2],[0],[-3],[],[],[],[]]
+ * <p>
+ * 输出：
+ * [null,null,null,null,-3,null,0,-2]
+ * <p>
+ * 解释：
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> 返回 -3.
+ * minStack.pop();
+ * minStack.top();      --> 返回 0.
+ * minStack.getMin();   --> 返回 -2.
+ * <p>
+ * <p>
+ * 方法一：辅助栈
+ * 思路
+ * <p>
+ * 要做出这道题目，首先要理解栈结构先进后出的性质。
+ * <p>
+ * 对于栈来说，如果一个元素 a 在入栈时，栈里有其它的元素 b, c, d，那么无论这个栈在之后经历了什么操作，只要 a 在栈中，b, c, d 就一定在栈中，因为在 a 被弹出之前，b, c, d 不会被弹出。
+ * <p>
+ * 因此，在操作过程中的任意一个时刻，只要栈顶的元素是 a，那么我们就可以确定栈里面现在的元素一定是 a, b, c, d。
+ * <p>
+ * 那么，我们可以在每个元素 a 入栈时把当前栈的最小值 m 存储起来。在这之后无论何时，如果栈顶元素是 a，我们就可以直接返回存储的最小值 m。
+ * <p>
+ * 当一个元素要入栈时，我们取当前辅助栈的栈顶存储的最小值，与当前元素比较得出最小值，将这个最小值插入辅助栈中；
+ * <p>
+ * 当一个元素要出栈时，我们把辅助栈的栈顶元素也一并弹出；
+ * <p>
+ * 在任意一个时刻，栈内元素的最小值就存储在辅助栈的栈顶元素中。
+ */
+public class solution01 {
+    class MinStack {
+        Deque<Integer> xStack;
+        Deque<Integer> minStack;
+
+        public MinStack() {
+            xStack = new ArrayDeque<Integer>();
+            minStack = new ArrayDeque<Integer>();
+            minStack.push(Integer.MAX_VALUE);
+        }
+
+        public void push(int x) {
+            xStack.push(x);
+            //每次都添加了栈顶元素和入栈元素的最小的
+            minStack.push(Math.min(minStack.peek(), x));
+        }
+
+        /**
+         * 因为每次都添加了栈顶元素和入栈元素的最小的
+         * */
+        public void pop() {
+            xStack.pop();
+            minStack.pop();
+        }
+
+        public int top() {
+            return xStack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
