61bb1ceea9fcb3a9bcb4df11efc238c5dd45872d
gaozhe <373795878@qq.com>
2021-09-15 23:18:44 +0800

    插入排序

--- src/com/algorithm/sorts/insertSort.java
+++ src/com/algorithm/sorts/insertSort.java
@@ -5,38 +5,39 @@ import java.util.Arrays;
 public class insertSort {
     /*
     * 直接插入排序(Straight Insertion Sort)的基本思想是：把n个待排序的元素看成为一个有序表和一个无序表。
-    * 开始时有序表中只包含1个元素，无序表中包含有n-1个元素，排序过程中每次从无序表中取出第一个元素，将它插入到有序表中的适当位置，使之成为新的有序表，重复n-1次可完成排序过程。
-    *
-    *
-    *
-    *
-     直接插入排序时间复杂度
-直接插入排序的时间复杂度是O(N2)。
-假设被排序的数列中有N个数。遍历一趟的时间复杂度是O(N)，需要遍历多少次呢？N-1！因此，直接插入排序的时间复杂度是O(N2)。
+    * 开始时有序表中只包含1个元素，无序表中包含有n-1个元素，排序过程中每次从无序表中取出第一个元素，
+    * 将它插入到有序表中的适当位置，使之成为新的有序表，重复n-1次可完成排序过程。
 
-    直接插入排序稳定性
-直接插入排序是稳定的算法，它满足稳定算法的定义。
-算法稳定性 -- 假设在数列中存在a[i]=a[j]，若在排序之前，a[i]在a[j]前面；并且排序之后，a[i]仍然在a[j]前面。则这个排序算法是稳定的！
+        直接插入排序时间复杂度:
+        直接插入排序的时间复杂度是O(N2)。
+        假设被排序的数列中有N个数。遍历一趟的时间复杂度是O(N)，需要遍历多少次呢？N-1！因此，直接插入排序的时间复杂度是O(N2)。
+
+        直接插入排序稳定性:
+        直接插入排序是稳定的算法，它满足稳定算法的定义。
+        算法稳定性 -- 假设在数列中存在a[i]=a[j]，若在排序之前，a[i]在a[j]前面；并且排序之后，a[i]仍然在a[j]前面。则这个排序算法是稳定的！
     * */
-    public static void InsertSort(int[] arr) {
-        /**
-         * temp是带插入的关键字，因为待插入的关键字紧挨前面的前面的关键字，
-         * 若前面的关键字往后移，会覆盖掉待插入的关键字，所以用temp来保存
-         *
-         * */
-
-        // 从第二个数开始，往前插入数字
-        for (int i = 1; i < arr.length; i++) {
-            int currentNumber = arr[i];
-            int j = i - 1;
-            // 寻找插入位置的过程中，不断地将比 currentNumber 大的数字向后挪
-            while (j >= 0 && currentNumber < arr[j]) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            // 两种情况会跳出循环：1. 遇到一个小于或等于 currentNumber 的数字，跳出循环，currentNumber 就坐到它后面。
-            // 2. 已经走到数列头部，仍然没有遇到小于或等于 currentNumber 的数字，也会跳出循环，此时 j 等于 -1，currentNumber 就坐到数列头部。
-            arr[j + 1] = currentNumber;
-        }
+    public static void main(String[] args) {
+        int[] nums = new int[]{5, 1, 3, 2, 10, 9};
+        InsertSort(nums);
+        System.out.println(Arrays.toString(nums));
     }
+
+    public static void InsertSort(int[] nums) {
+        if (nums.length < 2 || nums == null) {
+            return;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i - 1; j >= 0 && nums[j + 1] < nums[j]; j--) {
+                swap(nums, j, j + 1);
+            }
+        }
+
+    }
+
+    public static void swap(int[] nums, int i, int j) {
+        nums[i] = nums[i] ^ nums[j];
+        nums[j] = nums[i] ^ nums[j];
+        nums[i] = nums[i] ^ nums[j];
+    }
+
 }
