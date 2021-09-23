9de71757f126a06b727d91f799ac0146e0542966
gaozhe <373795878@qq.com>
2021-09-23 23:32:44 +0800

    快速排序

--- src/com/algorithm/sorts/quickSort.java
+++ src/com/algorithm/sorts/quickSort.java
@@ -5,83 +5,151 @@ import java.util.Arrays;
 public class quickSort {
 
     /*
-     快速排序(Quick Sort)使用分治法策略。
-它的基本思想是：选择一个基准数，通过一趟排序将要排序的数据分割成独立的两部分；其中一部分的所有数据都比另外一部分的所有数据都要小。
-然后，再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
-
-快速排序流程：
-(1) 从数列中挑出一个基准值。
-(2) 将所有比基准值小的摆放在基准前面，所有比基准值大的摆在基准的后面(相同的数可以到任一边)；在这个分区退出之后，该基准就处于数列的中间位置。
-(3) 递归地把"基准值前面的子数列"和"基准值后面的子数列"进行排序。
-
-
-
-    快速排序稳定性
-快速排序是不稳定的算法，它不满足稳定算法的定义。
-算法稳定性 -- 假设在数列中存在a[i]=a[j]，若在排序之前，a[i]在a[j]前面；并且排序之后，a[i]仍然在a[j]前面。则这个排序算法是稳定的！
-
-
-
-    快速排序时间复杂度
-快速排序的时间复杂度在最坏情况下是O(N2)，平均的时间复杂度是O(N*lgN)。
-这句话很好理解：假设被排序的数列中有N个数。遍历一次的时间复杂度是O(N)，需要遍历多少次呢？至少lg(N+1)次，最多N次。
-(1) 为什么最少是lg(N+1)次？快速排序是采用的分治法进行遍历的，我们将它看作一棵二叉树，它需要遍历的次数就是二叉树的深度，而根据完全二叉树的定义，它的深度至少是lg(N+1)。因此，快速排序的遍历次数最少是lg(N+1)次。
-(2) 为什么最多是N次？这个应该非常简单，还是将快速排序看作一棵二叉树，它的深度最大是N。因此，快读排序的遍历次数最多是N次。
-    * */
-
-    public static void main(String[] args) {
-        int[] arr = new int[]{4, 4, 6, 5, 3, 2, 8, 1};
-        quickSort(arr, 0, arr.length - 1);
-        System.out.println(Arrays.toString(arr));
-    }
-
-    public static void quickSort(int[] arr, int startIndex, int endIndex) {
-        // 递归结束条件：startIndex大于或等于endIndex时
-        if (startIndex >= endIndex) {
-            return;
-        }
-        // 得到基准元素位置
-        int pivotIndex = partition(arr, startIndex, endIndex);
-        // 根据基准元素，分成两部分进行递归排序
-        quickSort(arr, startIndex, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, endIndex);
-    }
-
-
-    /**
-     * 分治（双边循环法）
-     *
-     * @param arr        待交换的数组
-     * @param startIndex 起始下标
-     * @param endIndex   结束下标
+     * 快速排序(Quick Sort)使用分治法策略。
+     * 它的基本思想是：选择一个基准数，通过一趟排序将要排序的数据分割成独立的两部分；其中一部分的所有数据都比另外一部分的所有数据都要小。
+     * 然后，再按此方法对这两部分数据分别进行快速排序，整个排序过程可以递归进行，以此达到整个数据变成有序序列。
+     * 
+     * 快速排序流程： (1) 从数列中挑出一个基准值。 (2)
+     * 将所有比基准值小的摆放在基准前面，所有比基准值大的摆在基准的后面(相同的数可以到任一边)；在这个分区退出之后，该基准就处于数列的中间位置。 (3)
+     * 递归地把"基准值前面的子数列"和"基准值后面的子数列"进行排序。
+     * 
+     * 
+     * 
+     * 快速排序稳定性 快速排序是不稳定的算法，它不满足稳定算法的定义。 算法稳定性 --
+     * 假设在数列中存在a[i]=a[j]，若在排序之前，a[i]在a[j]前面；并且排序之后，a[i]仍然在a[j]前面。则这个排序算法是稳定的！
+     * 
+     * 
+     * 
+     * 快速排序时间复杂度 快速排序的时间复杂度在最坏情况下是O(N2)，平均的时间复杂度是O(N*lgN)。
+     * 这句话很好理解：假设被排序的数列中有N个数。遍历一次的时间复杂度是O(N)，需要遍历多少次呢？至少lg(N+1)次，最多N次。 (1)
+     * 为什么最少是lg(N+1)次？快速排序是采用的分治法进行遍历的，我们将它看作一棵二叉树，它需要遍历的次数就是二叉树的深度，而根据完全二叉树的定义，
+     * 它的深度至少是lg(N+1)。因此，快速排序的遍历次数最少是lg(N+1)次。 (2)
+     * 为什么最多是N次？这个应该非常简单，还是将快速排序看作一棵二叉树，它的深度最大是N。因此，快读排序的遍历次数最多是N次。
      */
-    private static int partition(int[] arr, int startIndex, int endIndex) {
-        // 取第1个位置（也可以选择随机位置）的元素作为基准元素
-        int pivot = arr[startIndex];
-        int left = startIndex;
-        int right = endIndex;
 
-        while (left != right) {
-            //控制right指针比较并左移
-            while (left < right && arr[right] > pivot) {
-                right--;
-            }
-            //控制left指针比较并右移
-            while (left < right && arr[left] <= pivot) {
-                left++;
-            }
-            //交换left和right指针所指向的元素
-            if (left < right) {
-                int p = arr[left];
-                arr[left] = arr[right];
-                arr[right] = p;
-            }
-        }
-        //pivot和指针重合点交换
-        arr[startIndex] = arr[left];
-        arr[left] = pivot;
-        return left;
-    }
+    public static void quickSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		quickSort(arr, 0, arr.length - 1);
+	}
+
+	public static void quickSort(int[] arr, int l, int r) {
+        //一定要写的终止条件
+		if (l < r) {
+			int[] p = partition(arr, l, r);
+            //将被比较数前后递归排序
+			quickSort(arr, l, p[0]);
+			quickSort(arr, p[1], r);
+		}
+	}
+
+	public static int[] partition(int[] arr, int l, int r) {
+		int less = l - 1;
+		int more = r;
+		while (l < more) {
+			if (arr[l] < arr[r]) {
+				swap(arr, ++less, l++);
+			} else if (arr[l] > arr[r]) {
+				swap(arr, --more, l);
+			} else {
+				l++;
+			}
+		}
+        //more的索引位置是被比较数，所以要将它前面的索引返回
+        //less就是被比较数前的索引
+		swap(arr, more, r);
+		return new int[] { less, more + 1 };
+	}
+
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	// for test
+	public static void comparator(int[] arr) {
+		Arrays.sort(arr);
+	}
+
+	// for test
+	public static int[] generateRandomArray(int maxSize, int maxValue) {
+		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+		}
+		return arr;
+	}
+
+	// for test
+	public static int[] copyArray(int[] arr) {
+		if (arr == null) {
+			return null;
+		}
+		int[] res = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			res[i] = arr[i];
+		}
+		return res;
+	}
+
+	// for test
+	public static boolean isEqual(int[] arr1, int[] arr2) {
+		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+			return false;
+		}
+		if (arr1 == null && arr2 == null) {
+			return true;
+		}
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// for test
+	public static void printArray(int[] arr) {
+		if (arr == null) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	// for test
+	public static void main(String[] args) {
+		int testTime = 500000;
+		int maxSize = 100;
+		int maxValue = 100;
+		boolean succeed = true;
+		for (int i = 0; i < testTime; i++) {
+			int[] arr1 = generateRandomArray(maxSize, maxValue);
+			int[] arr2 = copyArray(arr1);
+			quickSort(arr1);
+			comparator(arr2);
+			if (!isEqual(arr1, arr2)) {
+				succeed = false;
+				printArray(arr1);
+				printArray(arr2);
+				break;
+			}
+		}
+		System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+		int[] arr = generateRandomArray(maxSize, maxValue);
+		printArray(arr);
+		quickSort(arr);
+		printArray(arr);
+
+	}
 
 
 }
